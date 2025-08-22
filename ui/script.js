9b620e6bca48474673359dabd13f5d0914d24482
Smashitter2020 <smashitter2020@gmail.com>
2025-08-21 22:01:38 -0700

    Create script.js

---
+++ ui/script.js
@@ -0,0 +1,10 @@
+const input = document.getElementById('promptInput');
+const button = document.querySelector('#sendButton');
+
+input.addEventListener('input', () => {
+  if (input.value.trim() === "") {
+    button.disabled = true;
+  } else {
+    button.disabled = false;
+  }
+});
